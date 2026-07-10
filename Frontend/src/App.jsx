bc6a6fa8b0399b09d3b15286a159e3e4f4256852
Ledion124 <ledion.sakaj@lernende.bbw.ch>
2026-07-10 11:50:49 +0200

    frontend fertig

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,24 +2,79 @@ import { useEffect, useState } from "react";
 import { getPlayers } from "./api/players";
 import PlayerCard from "./components/PlayerCard";
 
+import "./App.css";
+
 function App() {
-  const [players, setPlayers] = useState([]);
+  const [cards, setCards] = useState([]);
+  const [player1, setPlayer1] = useState([]);
+  const [player2, setPlayer2] = useState([]);
 
   useEffect(() => {
-    getPlayers().then(setPlayers).catch(console.error);
+    getPlayers()
+        .then((apiCards) =>
+        {
+          setCards(apiCards);
+
+          if (apiCards.length !== 0) {
+            const mixed = [...apiCards].sort(() => Math.random() - 0.5);
+            const Mitte = Math.floor(mixed.length / 2);
+            const slice1 = mixed.slice(0, Mitte);
+            const slice2 = mixed.slice(Mitte);
+            setPlayer1(slice1);
+            setPlayer2(slice2);
+          }
+        })
   }, []);
 
-  return (
-    <main>
-      <h1>Football Quartett</h1>
+  console.log(player1);
+  console.log(player2);
 
-      <div className="cards">
-        {players.map((player) => (
-          <PlayerCard key={player.id} player={player} />
-        ))}
-      </div>
-    </main>
+function player1gewinnt(){
+  if(player2.length > 0) {
+    setPlayer1([...player1.slice(1), player2[0], player1[0]]);
+    setPlayer2(player2.slice(1))
+  }
+}
+function player2gewinnt(){
+  if(player1.length > 0) {
+    setPlayer2([...player2.slice(1), player1[0],player2[0]]);
+    setPlayer1(player1.slice(1));
+  }
+
+}
+
+  return (
+      <>
+        <main>
+          <h1>Football Quartett</h1>
+
+          <div className="layout">
+            <div className="spielfeld">
+              <div className="cards">
+                <h2>Stapel 1</h2>
+                {player1[0] && <PlayerCard key={player1[0].id} player={player1[0]} />}
+                <button onClick={player1gewinnt}>Player 1 gewinnt</button>
+              </div>
+
+              <div className="cards">
+                <h2>Stapel 2</h2>
+                {player2[0] && <PlayerCard key={player2[0].id} player={player2[0]} />}
+                <button onClick={player2gewinnt}>Player 2 gewinnt</button>
+              </div>
+            </div>
+
+
+            <div className="alle-karten">
+              <h2>Alle Karten</h2>
+              {cards.map(card => (
+                  <PlayerCard key={card.id} player={card} />
+              ))}
+            </div>
+          </div>
+        </main>
+      </>
   );
+
 }
 
 export default App;
--- Frontend/src/components/PlayerCard.jsx
+++ Frontend/src/components/PlayerCard.jsx
@@ -1,21 +1,23 @@
-export default function PlayerCard({ player }) {
-  return (
-    <div className="card">
-      <h2>{player.name}</h2>
-      <p>
-        {player.club} • {player.nation}
-      </p>
-      <p>Position: {player.position}</p>
 
-      <ul>
-        <li>Pace: {player.pace}</li>
-        <li>Shooting: {player.shooting}</li>
-        <li>Passing: {player.passing}</li>
-        <li>Dribbling: {player.dribbling}</li>
-        <li>Defending: {player.defending}</li>
-        <li>Physical: {player.physical}</li>
-        <li>Overall: {player.overall}</li>
-      </ul>
-    </div>
-  );
+export default function PlayerCard({ player }) {
+    return (
+        <div className="card">
+            <h2>{player.name}</h2>
+            <p className="subtitle">
+                {player.club} • {player.nation}
+            </p>
+            <p className="meta">Position: {player.position}</p>
+
+            <div className="stats">
+                <p><span>Pace</span> <span>{player.pace}</span></p>
+                <p><span>Shooting</span> <span>{player.shooting}</span></p>
+                <p><span>Passing</span> <span>{player.passing}</span></p>
+                <p><span>Dribbling</span> <span>{player.dribbling}</span></p>
+                <p><span>Defending</span> <span>{player.defending}</span></p>
+                <p><span>Physical</span> <span>{player.physical}</span></p>
+            </div>
+
+            <p className="overall"><span>Overall</span> <span>{player.overall}</span></p>
+        </div>
+    );
 }
